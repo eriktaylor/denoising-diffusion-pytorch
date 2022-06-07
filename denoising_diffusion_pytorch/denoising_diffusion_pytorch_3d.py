bcc3bd245cd123ac8bedb782d785416a49ef631d
Erik Taylor <erikntaylor@gmail.com>
2022-06-07 11:41:24 -0700

    Update denoising_diffusion_pytorch_3d.py

--- denoising_diffusion_pytorch/denoising_diffusion_pytorch_3d.py
+++ denoising_diffusion_pytorch/denoising_diffusion_pytorch_3d.py
@@ -692,13 +692,12 @@ class Trainer(object):
                     self.step_ema()
 
                 if self.step != 0 and self.step % self.save_and_sample_every == 0:
-                    self.ema_model.eval()
-
+                    #self.ema_model.eval()
                     milestone = self.step // self.save_and_sample_every
-                    batches = num_to_groups(36, self.batch_size)
-                    all_images_list = list(map(lambda n: self.ema_model.sample(batch_size=n), batches))
-                    all_images = torch.cat(all_images_list, dim=0)
-                    utils.save_image(all_images, str(self.results_folder / f'sample-{milestone}.png'), nrow = 6)
+                    #batches = num_to_groups(36, self.batch_size)
+                    #all_images_list = list(map(lambda n: self.ema_model.sample(batch_size=n), batches))
+                    #all_images = torch.cat(all_images_list, dim=0)
+                    #utils.save_image(all_images, str(self.results_folder / f'sample-{milestone}.png'), nrow = 6)
                     self.save(milestone)
                     
                     #ET -> added for colab & google drive use
